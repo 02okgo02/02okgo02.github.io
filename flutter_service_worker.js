1791f65505dea87965e850383c4bae4f6e1a3600
Aleksandr Yurkovskiy <sanekyy@gmail.com>
2020-11-26 00:52:51 +0300

    Storage methods

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "4b6db237b3514a88107a422469adfb0f",
-"index.html": "6fd67655164d0488395e983170b5613f",
-"/": "6fd67655164d0488395e983170b5613f",
-"main.dart.js": "3827b83701fad92ed932a2920af66084",
+"index.html": "f55a6926ada345a7653d6fe8ba756f1c",
+"/": "f55a6926ada345a7653d6fe8ba756f1c",
+"main.dart.js": "d3f3d095e07215e2d8176c7f0986e9b0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
