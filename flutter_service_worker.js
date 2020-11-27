ff6434c2914dd4f62342e585c62acd5ae49ffbbb
Aleksandr Yurkovskiy <sanekyy@gmail.com>
2020-11-27 19:35:10 +0300

    new methods ( version 1.2.0 )

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "4b6db237b3514a88107a422469adfb0f",
-"index.html": "7f19f1c3ef51a3d411f6abc63492c1ba",
-"/": "7f19f1c3ef51a3d411f6abc63492c1ba",
-"main.dart.js": "cb3fcde5ce79ea211c927bc33f9213d5",
+"index.html": "a722b213b6596bfcb44a54bad2642780",
+"/": "a722b213b6596bfcb44a54bad2642780",
+"main.dart.js": "07c3dc7151971742605a9cadd441a739",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
